0112fb35a785f01f9a281fe7a4cc58fd8db85073
VanesMP <vanessa.MP@outlook.fr>
2023-01-20 15:37:25 +0100

    installation material ui

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import arrowDown from "../assets/arrowDown.svg";
+import arrowUp from "../assets/ArrowUp.svg";
 import Background from "../Composnent/Background";
 import Footer from "../Composnent/Footerr";
 import Identity from "../Composnent/Identity";
@@ -13,16 +14,17 @@ function Home() {
             <Background />
             <div>
             <Identity />
-            <img src={arrowDown} alt="arrow to check directly all the projects"  className="arrow"/>
+            <a href="#ancreProject"><img src={arrowDown} alt="arrow to check directly all the projects"  className="arrow" /></a>
             </div>
             <div className="containerPresentation">
+                <div className="upPageProjects" id="ancreProject"></div>
+                <img src={arrowUp} alt="arrow to get back" className="arrowUp" />
             </div>
             <div className="headerPage">
                 <Name />
                 <Navbar />
             </div>
             <div className="arrowDown"></div>
-            
             <div className="footerPage">
                 <Footer />
             </div>
