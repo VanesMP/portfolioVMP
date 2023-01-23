79b0afb578c9f3592bc88759dabf3bdd9cd38f47
VanesMP <vanessa.MP@outlook.fr>
2023-01-23 14:58:26 +0100

    changes color job when scrolling

--- src/Composnent/Name.js
+++ src/Composnent/Name.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../Styles/name.css";
 
 
 function Name () {
+
+    const [textClass, setTextClass] = useState('default');
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+      
+      const handleScroll = () => {
+        if (window.scrollY > 860) {
+          setTextClass('scrolled');
+        } else {
+          setTextClass('default');
+        }
+      };
+      
+
     return (
         <div className="containerName">
             <h1 className="name">Vanessa Macedo Pinto</h1>
-            <h2 className="job">Développeuse Frontend Junior</h2>
+            <h2 className="job" id={textClass} >Développeuse Frontend Junior</h2>
         </div>
     )
 };
