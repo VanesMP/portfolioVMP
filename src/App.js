1049332810a5905f252a83a08dadbad2cecf4cff
VanesMP <vanessa.MP@outlook.fr>
2023-03-02 15:04:53 +0100

    changes route home

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     <div className="App">
     <Router>  
         <Routes>
+          <Route path="*" element={<Home />} />
           <Route exact path="/" element={<Home />} />
           <Route path="/projectPage/:id" element={<Project />} />
           <Route path="/about" element={<About />} />
