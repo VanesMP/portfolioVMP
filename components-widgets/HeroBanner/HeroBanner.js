7ea27ea66a564be6b050a890734347b7d6bde342
Vanessa Macedo <vanessa@MacBook-Pro-de-Vanessa.local>
2026-05-26 15:50:29 +0200

    fix style for image

--- components-widgets/HeroBanner/HeroBanner.js
+++ components-widgets/HeroBanner/HeroBanner.js
@@ -9,14 +9,16 @@ export const HeroBanner = () => {
 
   return (
     <div className={Styles.heroBanner}>
-      <Image
-        src="/images/identity/pict-dev.webp"
-        alt="identity picture"
-        width={256}
-        height={320}
-        priority
-        className={Styles.heroBanner__image}
-      />
+      <div className={Styles.heroBanner__imageContainer}>
+        <Image
+          src="/images/identity/pict-dev.webp"
+          alt="identity picture"
+          width={256}
+          height={320}
+          priority
+          className={Styles.heroBanner__image}
+        />
+      </div>
       <div className={Styles.heroBanner__content}>
         <h1 className={Styles.heroBanner__title}>{t("heroBanner.title")}</h1>
         <span className={Styles.heroBanner__subtitle}>{t("heroBanner.subtitle")}</span>
